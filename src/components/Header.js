2bfbe7a197297a9196508c592a3ea67ee1f5599c
Timofey Golovkov <tee-jay@yandex.ru>
2021-01-28 15:19:18 +0500

    До конца не разобрался, как передавать фоновую картинку через пропсы, а в остальном вроде всё работает )

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import s from "./Header.module.css"
+const Header = ({title, descr}) => {
+    return (
+        <header className={s.root}>
+            <div className={s.forest}/>
+            <div className={s.container}>
+                <h1>{title}</h1>
+                <p>{descr}</p>
+            </div>
+        </header>
+    )
+}
+export default Header;
